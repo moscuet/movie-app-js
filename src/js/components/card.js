e0115d19b4ae69d9895ad73625a714236a2868b1
AlvinBach <quocanh1990bach@gmail.com>
2021-03-20 23:27:20 +0200

    fixing movie__footer

--- src/js/components/card.js
+++ src/js/components/card.js
@@ -1,13 +1,13 @@
 import { starsRating } from '../utils/starsRating';
 
 export const card = (movies) => {
-  let cardsHtmlString = '';
-  movies.forEach(({ title, image, rating, releaseDate, genresString }) => {
-    cardsHtmlString += `        
+    let cardsHtmlString = '';
+    movies.forEach(({ title, image, rating, releaseDate, genresString }) => {
+        cardsHtmlString += `        
             <div class="movie">
-                <div class="movie__img__container">
-                    <img class="movie__img" src=${image} alt="${title} poster" />
-                </div>
+            <div class="movie__img__container">
+            <img class="movie__img" src=${image} alt="${title} poster">
+        </div>
                 <div class="movie__footer">
                     <div class="movie__title">${title}</div>                    
                     <div class="movie__rating__stars">
@@ -16,9 +16,11 @@ export const card = (movies) => {
                     <div class="movie__genres">${genresString}</div>
                     <div class="movie__date">${releaseDate}</div>
                 </div>
+                <i class="far fa-heart movie__card__icon"></i>
+                <i class="fas fa-heart moviecard__filled__icon"></i>
             </div>       
         `;
-  });
+    });
 
-  return cardsHtmlString;
+    return cardsHtmlString;
 };
